4df2f0666599a2cae4b72f8588c4f517cbd2ab50
Mohamed Raffik <raffikmohamed7@gmail.com>
2024-03-04 00:52:20 -0500

    fix templates sync

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ if [[ ! -d templates ]]; then
     mkdir templates
 fi
 
-rsync --recursive --ignore-existing base_templates templates/base
+rsync --recursive --ignore-existing base_templates/ templates/base
 
 poetry run gunicorn -w 4 --bind 0.0.0.0:80 notifcraft.app:app

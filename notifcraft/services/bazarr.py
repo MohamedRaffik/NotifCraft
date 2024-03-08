564b72e66838f22132d3e6a72e15784083b1823b
Mohamed Raffik <raffikmohamed7@gmail.com>
2024-03-07 19:40:34 -0500

    fix bazarr message splitting

--- notifcraft/services/bazarr.py
+++ notifcraft/services/bazarr.py
@@ -18,9 +18,10 @@ class DiscordMessageBuilder(BaseDiscordMessageBuilder):
         content: str = context.get("message")
         if ":" in content:
             last_index = 0
-            for i in range(len(content) - 1, -1):
+            for i in range(len(content) - 1, -1, -1):
                 if content[i] == ":":
                     last_index = i
+                    break
             media = content[:last_index]
             message = content[last_index + 1 :]
             return {"media": media.strip(), "message": message.strip()}
